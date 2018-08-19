375936375e1991b184ce346d72477f892d76f035
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-08-19 22:22:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,29 @@
-const Discord = require('discord.js');
-const A7MD = new Discord.Client();
-
-console.log("by M7mD_");
- 
-A7MD.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-سلام عليكم 
-لو سمحت ممكن تدخل سيرفرى
-قررنا افتتاح سيرفر بيع بوتات وتصاميم  
- [https://discord.gg/Bm9XqR ]							   
-الدعووة خاصة لك ... [ ${member}  ]
-**`) 
-}).catch(console.error)
-})
-A7MD.login('NDA1Nzk1NTk1NDUwNzEyMDgx.Dlh08Q.GdzGWKeX5firTRPxg6VcKEdFbck');
+client.on('message', message => {
+    if (message.content.startsWith("⫷༺public-chat༺⫸)) {
+                                    var mentionned = message.mentions.users.first();
+                var mentionavatar;
+                  if(mentionned){
+                      var mentionavatar = mentionned;
+                  } else {
+                      var mentionavatar = message.author;
+                      
+                  }
+                  let bot;
+                  if(message.author.bot) {
+                      bot = 'Bot'
+                  } else {
+                      bot = 'User'
+                  } 
+     var EsTeKnAN = new Discord.RichEmbed()
+     .setColor('RANDOM')
+     .setThumbnail(`${mentionavatar.avatarURL}`)
+     .addField("***شكرا الانضمامك الينا***" ,mentionavatar.username )
+     .setDescription('***|▬▬▬▬▬▬▬•ஜ۩۞۩ஜ•▬▬▬▬▬▬▬|
+                              :rose: ولكم منور  :rose:
+|▬▬▬▬▬▬▬•ஜ۩۞۩ஜ•▬▬▬▬▬▬▬|
+***')
+     .setImage('http://www.imgion.com/images/01/Welcome-buddy.jpg')
+      message.channel.sendEmbed(EsTeKnAN);
+     }
+   });
+client.on("NDgwODI3NDU0ODI3NzkwMzM3.Dlti_w.PjlOM-Lrbd7V0LU2A7bMoUSmDjI")
